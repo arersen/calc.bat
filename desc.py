0f4a613ac9d2243a4b45ed11f228ee59838016f4
arersen <71315641+arersen@users.noreply.github.com>
2020-12-02 19:49:47 +0200

    Update desc.py

--- desc.py
+++ desc.py
@@ -4,3 +4,11 @@ c = int(input("c="))
 D = ( b * b) - 4 * a * c
 print("D=",D)
 yn = input("Do you want get x1 and x2?    Y/N")
+if yn == "Y":
+  x1 = (-b + (D ** 1/2)) / 2*a
+  x2 = (-b - (D ** 1/2)) / 2*a
+  print("x1=",x1)
+  print("x2=",x2)
+  input("exit")
+
+
